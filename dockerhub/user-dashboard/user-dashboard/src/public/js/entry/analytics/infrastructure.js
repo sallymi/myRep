0a534e29ccb6d44a34a454eccdcb695a89ba4749
sally <lujjiang@cn.ibm.com>
2018-01-12 16:33:57 +0800

    try

---
+++ dockerhub/user-dashboard/user-dashboard/src/public/js/entry/analytics/infrastructure.js
@@ -0,0 +1,12 @@
+
+/* Copyright IBM Corp, All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Created by lixuc on 2017/5/15.
+ */
+requirejs(["../../common"], function(common) {
+    requirejs(["app/dashboard/analytics/infrastructure"]);
+});
---
+++ dockerhub/user-dashboard/user-dashboard/src/public/js/entry/index.js
@@ -0,0 +1,12 @@
+
+/* Copyright IBM Corp, All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Created by lixuc on 2017/5/2.
+ */
+requirejs(["../common"], function(common) {
+    requirejs(["app/index"]);
+});
