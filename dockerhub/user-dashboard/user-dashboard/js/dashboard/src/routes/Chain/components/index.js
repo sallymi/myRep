05b3676a2844c92439adb8799ddfb6b8d74e6f06
sally <lujjiang@cn.ibm.com>
2018-01-13 13:01:32 +0800

    move

--- dockerhub/user-dashboard/user-dashboard/js/dashboard/src/routes/Chain/components/index.js
+++
@@ -1,9 +0,0 @@
-/*
- SPDX-License-Identifier: Apache-2.0
-*/
-import EmptyView from './emptyView'
-// import ChainModal from './chainModal'
-import ChainView from './chainView'
-import EditModal from './editModal'
-
-export {EmptyView, ChainView, EditModal}
